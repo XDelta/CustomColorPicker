2434b889f38c6df72dc32fe81fa2535cd910d9c2
Delta <delta@deltawolf.tk>
2022-10-12 01:46:32 -0700

    Initial

---
+++ CustomColorPicker.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using BaseX;
+using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using SpecialItemsLib;
+
+namespace AvatarCreator {
+    public class AvatarCreator : NeosMod {
+        public override string Name => "CustomColorPicker";
+        public override string Author => "Delta";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/XDelta/CustomColorPicker/";
+
+        private static ModConfiguration Config;
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> enabled = new ModConfigurationKey<bool>("enabled", "Enables the mod", () => true);
+
+        public override void OnEngineInit() {
+            Config = GetConfiguration();
+            Config.Save(true);
+            Harmony harmony = new Harmony("tk.deltawolf.CustomColorPicker");
+            ColorPickerObject = SpecialItemsLib.SpecialItemsLib.RegisterItem(COLOR_PICKER_TAG);
+            harmony.PatchAll();
+        }
+        private static string COLOR_PICKER_TAG { get { return "custom_color_picker"; } }
+        private static CustomSpecialItem ColorPickerObject;
+
+        [HarmonyPatch(typeof(SlotHelper), "GenerateTags", new Type[] { typeof(Slot), typeof(HashSet<string>) })]
+        class SlotHelper_GenerateTags_Patch {
+            static void Postfix(Slot slot, HashSet<string> tags) {
+                if (slot.GetComponent<NeosColorDialog>() != null) {
+                    tags.Add(COLOR_PICKER_TAG);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(NeosColorDialog), "SetupColor")]
+        class NeosColorDialog_SetupColor_Patch {
+            static bool Prefix(NeosColorDialog __instance, color defaultColor) {
+                if (!Config.GetValue(enabled)) { return true; }
+                if (ColorPickerObject.Uri == null) { return true; }
+
+                var slot = __instance.Slot;
+
+                slot.StartTask(async delegate () {
+                    await slot.LoadObjectAsync(ColorPickerObject.Uri);
+                    InventoryItem component = slot.GetComponent<InventoryItem>();
+                    slot = ((component != null) ? component.Unpack() : null) ?? slot;
+                    slot.LocalScale = float3.One * 0.5f;
+
+                    NeosColorDialog colorDialog = slot.GetComponentInChildren<NeosColorDialog>();
+                    colorDialog.TargetField.Value = __instance.TargetField.Value;
+
+                    var originalColor = (AccessTools.Field(typeof(NeosColorDialog), "_originalColor").GetValue(colorDialog) as Sync<color>);
+                    originalColor.Value = defaultColor;
+
+                    slot.PositionInFrontOfUser(float3.Backward);
+
+                });
+                return false;
+            }
+        }
+    }
+}
